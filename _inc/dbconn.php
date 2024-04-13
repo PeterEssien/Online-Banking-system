a1540fbc5325a88fa49e8431cf95bf23af9a13a6
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2024-04-13 09:21:39 +0100

    Update dbconn.php

--- _inc/dbconn.php
+++ _inc/dbconn.php
@@ -1,8 +1,8 @@
 <?php
-$serverName="localhost";
-$dbusername="root";
-$dbpassword="rash";
-$dbname="bank_db";
+$serverName="wwv.h.filess.io";
+$dbusername="bankdb_cornoldest";
+$dbpassword="3cc3d7dfc88f69cda32656c885b969a5b587a5ea";
+$dbname="bankdb_cornoldest";
 mysql_connect($serverName,$dbusername,$dbpassword)/* or die('the website is down for maintainance')*/;
 mysql_select_db($dbname) or die(mysql_error());
 ?>
