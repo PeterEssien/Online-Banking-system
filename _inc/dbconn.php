d4fb3cba99ec865fe4d42737877a60bea4a2a455
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2024-04-13 09:35:07 +0100

    Update dbconn.php

--- _inc/dbconn.php
+++ _inc/dbconn.php
@@ -1,8 +1,10 @@
 <?php
-$serverName="wwv.h.filess.io";
-$dbusername="bankdb_cornoldest";
-$dbpassword="3cc3d7dfc88f69cda32656c885b969a5b587a5ea";
-$dbname="bankdb_cornoldest";
-mysql_connect($serverName,$dbusername,$dbpassword)/* or die('the website is down for maintainance')*/;
-mysql_select_db($dbname) or die(mysql_error());
+// Enter your Host, username, password, database below.
+// I left password empty because i do not set password on localhost.
+$con = mysqli_connect("wwv.h.filess.io","bankdb_cornoldest","3cc3d7dfc88f69cda32656c885b969a5b587a5ea","bankdb_cornoldest","3306");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
 ?>
